982647ffd2fb439b64b94fd79de7c5ce567222c9
Rudal-XD <106883384+Rudal-XD@users.noreply.github.com>
2023-08-11 11:56:26 +0700

    Update Run.py

--- Run.py
+++ Run.py
@@ -395,18 +395,18 @@ def ua():
 	
 def uas(__Aang__Sayang__Laura__):
 	if __Aang__Sayang__Laura__ == '':
-		print ('\n[!] Yang bener kontol');jeda(2)
+		print ('\n[!] Yang bener kontol');time.sleep(2)
 		uas(__Aang__Sayang__Laura__)
 	elif __Aang__Sayang__Laura__ in("1","01"):
 		print ("[!] Ketik cancel untuk gunakan ua dari script")
 		ua = input("[!] User agent : ")
 		if ua in(""):
-			print ('\n[!] Yang bener kontol');jeda(2)
+			print ('\n[!] Yang bener kontol');time.sleep(2)
 			daftar_menu()
 		elif ua in("CANCEL","Cancel","cancel"):
 			ua_ = ("Mozilla/5.0 (Linux; Android 10; Mi 9T Pro Build/QKQ1.190825.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/88.0.4324.181 Mobile Safari/537.36[FBAN/EMA;FBLC/it_IT;FBAV/239.0.0.10.109;]")
-			open("ua.txt","w").write(ua_);jeda(2)
-			print ("\n[✓]  Berhasil menggunakan user agent script ");jeda(2)
+			open("ua.txt","w").write(ua_);time.sleep(2)
+			print ("\n[✓]  Berhasil menggunakan user agent script ");time.sleep(2)
 			pilihan().daftar_menu()
 		open("ua.txt","w").write(ua);time.sleep(2)
 		print ("\n[✓] Berhasil mengganti user agent");time.sleep(2)
