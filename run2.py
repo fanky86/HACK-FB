ce35960958113f0b91469923d17b4bb0b52c7339
Rudal-XD <106883384+Rudal-XD@users.noreply.github.com>
2023-08-11 22:44:50 +0700

    Update run2.py

--- run2.py
+++ run2.py
@@ -235,7 +235,7 @@ class dump:
                     self.publik(userid, cookie, self.unit_cursor)
                     
                 else:
-                    setting()
+                    generate_password()
         except (KeyboardInterrupt):
             Console().print(f"[bold hot_pink2]   ╰─>[bold yellow] KeyboardInterrupt!          ", end='\r');time.sleep(3.6)
             return 3
