13e0015d14c4ad92e65b88db84c6c111c3692ae0
Rudal-XD <106883384+Rudal-XD@users.noreply.github.com>
2023-08-25 07:38:42 +0700

    Update run.py

--- run.py
+++ run.py
@@ -619,8 +619,8 @@ def massal():
 	except IOError:
 		exit()
 	try:
-		Console(width=80, style="bold cyan").print(Panel('\t            [bold white]Ketik [bold green]Me[/] Jika Ingin Crack Pertemanan Sendiri',subtitle="╭───", subtitle_align="left", title="[bold green]>[hot_pink2] (Crack Masal) [bold green]<"))
-		jum = int(input(f"[O][+] Mau Berapa Idz Target  ╰─> "))
+		Console(width=80, style="bold cyan").print(Panel('\t[bold white][+] Mau Berapa Idz Target',subtitle="╭───", subtitle_align="left", title="[bold green]>[hot_pink2] (Crack Masal) [bold green]<"))
+		jum = int(input(f"{O}  ╰─> "))
 	except ValueError:
 		print(' [+] Wrong input ')
 		exit()
