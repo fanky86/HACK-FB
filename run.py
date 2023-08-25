1c584e27e494be57a15cd3c22a44ed1b263e731c
Rudal-XD <106883384+Rudal-XD@users.noreply.github.com>
2023-08-25 20:28:52 +0700

    Update run.py

--- run.py
+++ run.py
@@ -952,7 +952,7 @@ def reguler1(idf,pwv):
 	ua = random.choice(ugen)
 	ua2 = random.choice(ugen2)
 	ses = requests.Session()
-	prog.update(f"[bold green]CRACK[bold white] {loop}/{len(id)} OK-:[bold green]{ok}[/] CP-:[bold yellow]{cp}[/]")
+	prog.update(des,description=f"[bold green]CRACK[bold white] {loop}/{len(id)} OK-:[bold green]{ok}[/] CP-:[bold yellow]{cp}[/]")
 	prog.advance(des) 
 	for pw in pwv:
 		try:
