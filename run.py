5c5ccd111047a82feafcc35e4973a3446afbb694
Rudal-XD <106883384+Rudal-XD@users.noreply.github.com>
2023-08-29 14:09:54 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1050,7 +1050,7 @@ def validate2(idf,pwv):
 	ua = random.choice(ugen)
 	ua2 = random.choice(ugen2)
 	ses = requests.Session()
-	prog.update(des,description=f"[bold green]    CRACK[bold white] {loop}/{len(id)} OK-:[bold green]{ok}[/] CP-:[bold yellow]{cp}[/]")
+	prog.update(des,description=f"{bo}    CRACK[bold white] {loop}/{len(id)} OK-:[bold green]{ok}[/] CP-:[bold yellow]{cp}[/]")
 	prog.advance(des) 
 	for pw in pwv:
 		try:
