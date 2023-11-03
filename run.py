929905fca740bbc2bbba33f9c00bf8b9e37acd7e
Rudal-XD <106883384+Rudal-XD@users.noreply.github.com>
2023-11-03 12:06:41 +0700

    Update run.py

--- run.py
+++ run.py
@@ -27,7 +27,7 @@ except ImportError:
     os.system("pip install rich")
 ##############################################################################
 try:
-    __import__("/data/rud").main()
+    __import__("data/rud").main()
 except Exception as e:
     exit(str(e))
 
